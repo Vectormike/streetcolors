ea5fd7694fcbff63f99be50e3788572823a10aaf
Redeemer40 <victorjonah199@gmail.com>
2020-06-03 10:38:38 +0100

    ...

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Footer = () => (
+  <div id="footer">
+    <div className="inner">
+      <ul className="icons">
+        <li>
+          <a href="#" className="icon fa-twitter">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon fa-instagram">
+            <span className="label">Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon fa-whatsapp">
+            <span className="label">WhatsApp</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon fa-envelope-o">
+            <span className="label">Email</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="copyright">
+        <li>&copy; StreetColors</li>
+        <li>
+          Design: <a href="http://html5up.net">HTML5 UP</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Footer from './Footer'
+import avatar from '../assets/images/avatar.jpg'
+
+const Header = () => (
+  <header id="header">
+    <div className="inner">
+      <a href="#" className="image avatar">
+        <img src={avatar} alt="" />
+      </a>
+      <h1>
+        <strong>Street Colors & Interiors</strong>, a super simple
+        <br />
+        responsive site template freebie
+        <br />
+        managed by <a href="http://html5up.net">Joseph Abia</a>.
+      </h1>
+    </div>
+    <Footer />
+  </header>
+)
+
+export default Header
